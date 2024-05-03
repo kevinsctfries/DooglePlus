61e88e8e6e53d84e42cdd9b057000c6c13b678ed
Kevin Fries <kevinsctfries@gmail.com>
2024-05-03 13:35:51 -0400

    correctly fetches followed/follower posts

--- api/contollers/post.js
+++ api/contollers/post.js
@@ -1,10 +1,29 @@
 import { db } from "../connect.js";
+import jwt from "jsonwebtoken";
 
 export const getPosts = (req, res) => {
-  const q = `SELECT p.*, u.id AS userId, name, profilePic FROM posts AS p JOIN users AS u ON (u.id = p.userId)`;
+  // const userId = req.query.userId;
+  const token = req.cookies.accessToken;
 
-  db.query(q, (err, data) => {
-    if (err) return res.status(500).json(err);
-    return res.status(200).json(data);
+  if (!token) return res.status(401).json("User not logged in");
+
+  jwt.verify(token, "secretkey", (err, userInfo) => {
+    if (err) return res.status(403).json("Invalid Access Token");
+
+    const actualUserId = userInfo.id.id; //TODO fix this
+
+    console.log("User ID from token: ", actualUserId);
+
+    const q = `SELECT p.*, u.id AS userId, name, profilePic FROM posts AS p JOIN users AS u ON (u.id = p.userId)
+    LEFT JOIN relationships AS r ON (p.userId = r.followedUserId) WHERE r.followerUserId = ? OR p.userId = ?
+    ORDER BY p.createdAt DESC`;
+
+    db.query(q, [actualUserId, actualUserId], (err, data) => {
+      if (err) {
+        console.error("Database error:", err);
+        return res.status(500).json("Database error: " + err.message); // More specific error message
+      }
+      return res.status(200).json(data);
+    });
   });
 };
