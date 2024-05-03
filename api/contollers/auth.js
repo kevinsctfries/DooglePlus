e94a94ce886108cf5e58364aff04cd53d9823a2f
Kevin Fries <kevinsctfries@gmail.com>
2024-05-03 16:09:28 -0400

    fixed userInfo

--- api/contollers/auth.js
+++ api/contollers/auth.js
@@ -44,7 +44,7 @@ export const login = (req, res) => {
 
     if (!checkPassword) return res.status(400).json("Wrong password/username!");
 
-    const token = jwt.sign({ id: data[0] }, "secretkey");
+    const token = jwt.sign({ id: data[0].id }, "secretkey");
     const { password, ...others } = data[0];
     res
       .cookie("accessToken", token, {
--- api/contollers/post.js
+++ api/contollers/post.js
@@ -12,13 +12,13 @@ export const getPosts = (req, res) => {
 
     const actualUserId = userInfo.id.id; //TODO fix this
 
-    console.log("User ID from token: ", actualUserId);
+    console.log("User ID from token: ", userInfo.id);
 
     const q = `SELECT p.*, u.id AS userId, name, profilePic FROM posts AS p JOIN users AS u ON (u.id = p.userId)
     LEFT JOIN relationships AS r ON (p.userId = r.followedUserId) WHERE r.followerUserId = ? OR p.userId = ?
     ORDER BY p.createdAt DESC`;
 
-    db.query(q, [actualUserId, actualUserId], (err, data) => {
+    db.query(q, [userInfo.id, userInfo.id], (err, data) => {
       if (err) {
         console.error("Database error:", err);
         return res.status(500).json("Database error: " + err.message); // More specific error message
